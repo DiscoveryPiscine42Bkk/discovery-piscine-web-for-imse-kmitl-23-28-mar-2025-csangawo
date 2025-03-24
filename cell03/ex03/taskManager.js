4abfde7de0af2c84f5fe8061a002e10e59c6e009
csangawo <67160007@kmitl.ac.th>
2025-03-24 18:12:02 +0700

    modified:   cell03/ex03/index.html 	renamed:    cell03/ex03/todo.js -> cell03/ex03/taskManager.js

--- cell03/ex03/taskManager.js
+++ cell03/ex03/taskManager.js
@@ -1,4 +1,4 @@
-// Function to load tasks from cookies
+
 function loadTasks() {
     const tasks = getCookie("tasks");
     if (tasks) {
@@ -9,7 +9,7 @@ function loadTasks() {
     }
 }
 
-// Function to save tasks to cookies
+
 function saveTasks() {
     const tasks = [];
     const taskDivs = document.querySelectorAll('#ft_list div');
@@ -19,7 +19,7 @@ function saveTasks() {
     setCookie("tasks", JSON.stringify(tasks), 365);
 }
 
-// Function to set a cookie
+
 function setCookie(name, value, days) {
     const date = new Date();
     date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
@@ -27,7 +27,7 @@ function setCookie(name, value, days) {
     document.cookie = name + "=" + value + ";" + expires + ";path=/";
 }
 
-// Function to get a cookie by name
+
 function getCookie(name) {
     const decodedCookies = decodeURIComponent(document.cookie);
     const cookies = decodedCookies.split(';');
@@ -40,33 +40,33 @@ function getCookie(name) {
     return "";
 }
 
-// Function to add a new task to the DOM
+
 function addTaskToDOM(task) {
     const taskDiv = document.createElement('div');
     taskDiv.textContent = task;
     taskDiv.classList.add('task');
     document.getElementById('ft_list').prepend(taskDiv);
 
-    // Add event listener to remove task on click
+
     taskDiv.addEventListener('click', function () {
         const confirmRemove = confirm("Do you want to remove this task?");
         if (confirmRemove) {
             taskDiv.remove();
-            saveTasks(); // Save updated tasks to cookies
+            saveTasks(); 
         }
     });
 }
 
-// Event listener for the "New" button
+
 document.getElementById('newButton').addEventListener('click', function () {
     const newTask = prompt("Enter your new task:");
     if (newTask && newTask.trim() !== "") {
         addTaskToDOM(newTask);
-        saveTasks(); // Save the new task to cookies
+        saveTasks(); 
     } else {
         alert("Task cannot be empty!");
     }
 });
 
-// Load tasks when the page is loaded
+
 loadTasks();
